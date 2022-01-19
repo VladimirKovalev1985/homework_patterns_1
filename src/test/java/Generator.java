555d543d59bc2219607e834f8a14c1d263271b08
Vladimir Kovalev <boban_envorter@mail.ru>
2022-01-19 19:58:34 +0300

    add generator

---
+++ src/test/java/Generator.java
@@ -0,0 +1,31 @@
+import com.github.javafaker.Faker;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class Generator {
+    private Generator(){}
+
+    private static Faker faker = new Faker(new Locale("ru"));
+
+    public static String generateDate(int days){
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String generateCity(String locale){
+        String[] Cities = new String[]{"Санкт-Петербург", "Омск", "Шерегеш", "Новосибирск", "Уфа", "Москва"};
+        int city = (int) Math.floor(Math.random() * Cities.length);
+        return Cities[city];
+    }
+
+    public static String generateName(String locale){
+        return faker.name().fullName();
+    }
+
+    public static String generatePhone(String locale){
+        return faker.phoneNumber().phoneNumber();
+    }
+
+
+}
