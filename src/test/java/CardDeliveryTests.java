56315290118395b5fa86bf7a7378f60e85b35b6d
Vladimir Kovalev <boban_envorter@mail.ru>
2022-01-20 20:41:02 +0300

    add test

---
+++ src/test/java/CardDeliveryTests.java
@@ -0,0 +1,31 @@
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardDeliveryTests {
+
+    @Test
+    public void shouldSendForm() {
+        open("http://localhost:9999/");
+        $("[data-test-id=city] input").setValue(Generator.generateCity("ru"));
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(Generator.generateDate(3));
+        $("[data-test-id=name] input").setValue(Generator.generateName("ru"));
+        $("[data-test-id=phone] input").setValue(Generator.generatePhone("ru"));
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Запланировать")).click();
+        $(".notification__content").shouldBe(visible,
+                exactText("Встреча успешно запланирована на " + Generator.generateDate(3)));
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(Generator.generateDate(4));
+        $$("button").find(exactText("Запланировать")).click();
+        $("[data-test-id=replan-notification]").shouldBe(visible);
+        $$("button").find(exactText("Перепланировать")).click();
+        $(".notification__content ").shouldBe(visible,
+                exactText("Встреча успешно запланирована на " + Generator.generateDate(4)));
+
+    }
+}
